78c6ce3cb38e8c1403f556133ae93d7d66122394
ArchieTramp <64407929+ArchieTramp@users.noreply.github.com>
2020-05-05 17:30:23 +0500

    final update with javadoc

--- src/main/java/MathBox.java
+++ src/main/java/MathBox.java
@@ -1,29 +1,44 @@
+/**
+ * @author Artur Gilyazov
+ * MathBox
+ * Написать класс MathBox, реализующий следующий функционал:
+ * Входной массив любого размера
+ * методы Summator, Splitter, Eraser
+ */
+
+
 import java.util.*;
 
 public class MathBox extends ObjectBox {
     public static void main(String [] args) {
+        /** объявление размера массива */
         try {
         int z;
         System.out.print("Введите размер массива ");
         Scanner scan = new Scanner(System.in);
         int capacity = scan.nextInt();
-        Integer[] numarray = new Integer[capacity]; //новый массив
+        Integer[] numarray = new Integer[capacity];
         Scanner scanarr = new Scanner(System.in);
+            /** ввод элементов массива*/
             for (z = 0; z < capacity; z++) {
             System.out.print("Введите значение для элемента " + z + " - ");
             numarray[z] = scanarr.nextInt();
             }
-        Arrays.sort(numarray); //сортировка для красоты
-        Set<Integer> numset = new HashSet<>(Arrays.asList(numarray)); //массив в сет, убирает повторы
-        List<Integer> numlist = new ArrayList<>(numset); //сет в лист для удобства работы
-        ArrayList<Integer> numbers = new ArrayList<>(numlist); //возврат в массив
+            /** сортировка исключительно для красоты отображения, которая все равно не будет слиять на Set */
+        Arrays.sort(numarray);
+        /** массив в сет только ради удаления повторов, ибо быстро */
+        Set<Integer> numset = new HashSet<>(Arrays.asList(numarray));
+        /** сет в лист, а потом в массив для удобства работы, вероятно можно было обойтись, но пока не уверен */
+        List<Integer> numlist = new ArrayList<>(numset);
+        ArrayList<Integer> numbers = new ArrayList<>(numlist);
 
         System.out.println("Отсортировано " + Arrays.toString(numarray));
         System.out.println("Удаляем повторы " + numset);
-
+        /** запуск методов */
         new MathBox(numbers).summator();
         new MathBox(numbers).splitter();
         new MathBox(numbers).eraser();
+        /** не уверен нужно ли оно тут, но запуск родительских методов в наследнике */
         new MathBox(numbers).addObject();
         new MathBox(numbers).deleteObject();} catch (Exception e) {
             System.out.println(e + " вводить необходимо целочисленные значения");
@@ -37,7 +52,7 @@ public class MathBox extends ObjectBox {
             this.numbers = numbers.toArray(new Integer[0]);
     }
 
-
+    /** Summator складывает все элементы массива */
     public int summator() {
         int i;
         int sum = 0;
@@ -48,7 +63,7 @@ public class MathBox extends ObjectBox {
         return sum;
 
     }
-
+    /** Splitter делит каждый элемент массива на аргумент 2, заменяя значения и выводя на экран */
     public int splitter() {
         int j;
         String[] split = new String[numbers.length];
@@ -59,7 +74,7 @@ public class MathBox extends ObjectBox {
         System.out.println("Поделим каждый элемент массива на 2 и получим " + Arrays.toString(split));
         return j;
     }
-
+    /** Eraser удаляет элемент из сканера, если такой есть в массиве */
     public int eraser() {
 
         System.out.print("Введите значение для удаления - ");
--- src/main/java/ObjectBox.java
+++ src/main/java/ObjectBox.java
@@ -1,18 +1,27 @@
-import java.util.*;
-import java.util.function.Predicate;
+/**
+ * @author Artur Gilyazov
+ * ObjectBox
+ * Написать класс ObjectBox, реализующий следующий функционал:
+ * добавление элемента в коллекцию
+ * удаление элемента из коллекции
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class ObjectBox {
 
     public static void main(String[] args) {
-
+        /** создание пустой коллекции */
         ArrayList<Object> objects = new ArrayList<>(Collections.emptyList());
-
+        /** выполнение методов */
         objects.add(addObject());
         System.out.println(objects);
         objects.remove(deleteObject());
         System.out.println(objects);
     }
-
+        /** addObject добавляет элемент в коллекцию */
         public static Object addObject() {
 
             System.out.print("Новый объект ");
@@ -20,7 +29,7 @@ public class ObjectBox {
             String x = scan.nextLine();
             return x;
         }
-
+        /** deleteObject удаляет элемент из коллекции */
         public static Object deleteObject() {
             System.out.print("Проверить объект ");
             Scanner scan = new Scanner(System.in);
