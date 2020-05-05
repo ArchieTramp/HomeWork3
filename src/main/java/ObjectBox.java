16bfcc26cc20c9c75dc306446dbac996aec84f08
ArchieTramp <64407929+ArchieTramp@users.noreply.github.com>
2020-05-05 16:19:37 +0500

    update

--- src/main/java/ObjectBox.java
+++ src/main/java/ObjectBox.java
@@ -4,22 +4,14 @@ import java.util.function.Predicate;
 public class ObjectBox {
 
     public static void main(String[] args) {
-//        Object[] arr = /* {"qwe", "wer", "ert", "tyu"};*/ new String[10];
+
         ArrayList<Object> objects = new ArrayList<>(Collections.emptyList());
-//        System.out.println("исходный массив " + Arrays.toString());
 
         objects.add(addObject());
         System.out.println(objects);
         objects.remove(deleteObject());
         System.out.println(objects);
-
     }
-
-//    private static String objects;
-//
-//    public ObjectBox(Object objects) {
-//        this.objects = objects.toString();
-//    }
 
         public static Object addObject() {
 
@@ -27,7 +19,6 @@ public class ObjectBox {
             Scanner scan = new Scanner(System.in);
             String x = scan.nextLine();
             return x;
-
         }
 
         public static Object deleteObject() {
@@ -35,16 +26,5 @@ public class ObjectBox {
             Scanner scan = new Scanner(System.in);
             String y = scan.nextLine();
             return y;
-
-
-
         }
-
-
-//    private void deleteObject() {
-//
-//    }
-
-
-
 }
